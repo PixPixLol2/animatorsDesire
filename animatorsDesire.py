f2e9a1da6d283751f259382e062d9b4dc8285e4c
PixPixLol2 <yayitstoday22@gmail.com>
2024-10-10 13:06:00 -0500

    Add files via upload

--- animatorsDesire.py
+++ animatorsDesire.py
@@ -1,31 +1,67 @@
 import pygame
-import os
-
+import time
+import math
 
 pygame.init()
+clock = pygame.time.Clock()
 
-screen_width = 1920
-screen_height = 1080
-screen = pygame.display.set_mode((screen_width, screen_height))
+W_W = 1920
+W_H = 1080
+BRUSH_SIZE = 2
+BRUSH_COLOR = (0, 0, 0)
 
-showSplashScreen = False
+screen = pygame.display.set_mode((W_W, W_H))
+
 splashImage = pygame.image.load("desireCreativeLogo.png").convert()
-image_rect = splashImage.get_rect()
+image_rect = splashImage.get_rect(center=(W_W // 2, W_H // 2))
 
+showSplashScreen = True
+splash_duration = 30
+splash_start_time = time.time()
 
+showAnimEditor = False
+
+def animEditor():
+    pass
+    
+def brushTool(screen, color, startPos, endPos, size):
+    distance = math.hypot(endPos[0] - startPos[0], endPos[1] - startPos[1])
+    circles = int(distance / size) + 1
+    for i in range(circles):
+        x = startPos[0] + (endPos[0] - startPos[0]) * i / circles
+        y = startPos[1] + (endPos[1] - startPos[1]) * i / circles
+        pygame.draw.circle(screen, color, (int(x), int(y)), size)
 
 running = True
+prevPos = None
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if running == True:
-            showSplashScreen = True
 
-    screen.fill((0, 0, 0))
+    mousePressed = pygame.mouse.get_pressed()
+    mousePos = pygame.mouse.get_pos()
+    
+    if mousePressed[0]:
+        if prevPos is None:
+            prevPos = mousePos
+        brushTool(screen, BRUSH_COLOR, prevPos, mousePos, BRUSH_SIZE)
+        prevPos = mousePos
+    else:
+        prevPos = None
+        
+    if showSplashScreen:
+        current_time = time.time()
+        if current_time - splash_start_time > splash_duration:
+            showSplashScreen = False
+            showAnimEditor = True
 
-    screen.blit(splashImage, image_rect)
+    screen.fill((255, 255, 255))
+
+    if showSplashScreen:
+        screen.blit(splashImage, image_rect)
 
     pygame.display.flip()
-    
+    clock.tick(15)
+
 pygame.quit()
